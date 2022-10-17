3383ea964ce3801ff3165ccbc1f833f1e0237e2a
stirelshka8 <stirelshka8@gmail.com>
2022-10-17 22:21:15 +0300

    Конфигурационный файл

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-import psycopg2, configparser, os.path
+import psycopg2, configparser, os
 
-if os.path.exists("configuration.ini"):
-    pass
-else:
-    file_path = open("configuration.ini", "w+")
-    file_path.write("""#Файл конфигурации подключения к базе данных PostgreSQL!
+os.system("clear")
+
+def performance():
+    config = configparser.ConfigParser()
+    config.read("configuration.ini")
+    namedb = config["DATABASE"]["NAME"]
+    userdb = config["DATABASE"]["USER"]
+    passdb = config["DATABASE"]["PASSWORD"]
+    hostdb = config["DATABASE"]["HOST"]
+
+    print(namedb, userdb,passdb,hostdb)
+
+def start_programm():
+    if os.path.exists("configuration.ini") == True:
+        performance()
+    else:
+        file_path = open("configuration.ini", "w+")
+        file_path.write("""#Файл конфигурации подключения к базе данных PostgreSQL!
 
 [DATABASE]
 NAME=None
 USER=None
 PASSWORD=None
 HOST=None""")
-print("Был создан конфигурационный файл базы данных. Откройте его и настройте параметры подключения!")
+        print("Был создан конфигурационный файл базы данных (configuration.ini). Откройте его и настройте параметры подключения!")
 
-
-#config = configparser.ConfigParser()
-#config.read("configuration.ini")
-#namedb = config["DATABASE"]["NAME"]
-#userdb = config["DATABASE"]["USER"]
-#passdb = config["DATABASE"]["PASSWORD"]
-#hostdb = config["DATABASE"]["HOST"]
-
-
-
-#print(namedb, userdb,passdb,hostdb)
+if __name__ == "__main__":
+    start_programm()
