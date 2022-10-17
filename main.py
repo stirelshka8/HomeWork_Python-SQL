78b23c66f53d72c5d42c9c697544e3b16558ea59
stirelshka8 <stirelshka8@gmail.com>
2022-10-17 21:49:30 +0300

    work

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello World')
+import psycopg2, configparser, os.path
+
+if os.path.exists("configuration.ini"):
+    pass
+else:
+    file_path = open("configuration.ini", "w+")
+    file_path.write("""#Файл конфигурации подключения к базе данных PostgreSQL!
+
+[DATABASE]
+NAME=None
+USER=None
+PASSWORD=None
+HOST=None""")
+print("Был создан конфигурационный файл базы данных. Откройте его и настройте параметры подключения!")
+
+
+#config = configparser.ConfigParser()
+#config.read("configuration.ini")
+#namedb = config["DATABASE"]["NAME"]
+#userdb = config["DATABASE"]["USER"]
+#passdb = config["DATABASE"]["PASSWORD"]
+#hostdb = config["DATABASE"]["HOST"]
+
+
+
+#print(namedb, userdb,passdb,hostdb)
