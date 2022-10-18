d12f04e479058b76930f43529b9f526ad3a6394c
stirelshka8 <stirelshka8@gmail.com>
2022-10-18 20:00:53 +0300

    decor

--- main.py
+++ main.py
@@ -13,27 +13,34 @@ def performance():
     userdb = config["DATABASE"]["USER"]
     passdb = config["DATABASE"]["PASSWORD"]
     hostdb = config["DATABASE"]["HOST"]
+    portdb = config["DATABASE"]["PORT"]
 
-    print(namedb, userdb,passdb,hostdb)
 
 def start_programm():
     if os.path.exists(configpath) == True:
+        print(f"[INFO] Программа запущена!.")
         performance()
     else:
         config.add_section("DATABASE")
-        add_dbname = input("[!]Введите имя базы данных - ")
-        add_dbuser = input("[!]Введите имя пользователя базы данных - ")
-        add_dbpass = input("[!]Введите пароль базы данных - ")
-        add_dbhost = input("[!]Введите адрес хоста базы данных - ")
+        add_dbname = input("[SET]Введите имя базы данных - ")
+        add_dbuser = input("[SET]Введите имя пользователя базы данных - ")
+        add_dbpass = input("[SET]Введите пароль базы данных - ")
+        add_dbhost = input("[SET]Введите адрес хоста базы данных - ")
+        add_dbport = input("[SET]Введите номер порта базы данных (по умолчанию - 5432) - ")
 
         config.set("DATABASE","NAME", add_dbname)
         config.set("DATABASE","USER", add_dbuser)
         config.set("DATABASE","PASSWORD", add_dbpass)
         config.set("DATABASE","HOST", add_dbhost)
-
+        config.set("DATABASE","PORT", add_dbport)
 
         with open(configpath, "w") as config_file:
             config.write(config_file)
+
+        os.system("clear")
+        print(f"[INFO] Файл конфигурации {configpath} создан.")
+        print(f"[INFO] Программа запущена!.")
+        performance()
 
 if __name__ == "__main__":
     start_programm()
